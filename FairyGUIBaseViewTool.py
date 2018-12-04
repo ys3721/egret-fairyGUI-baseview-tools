639f8c3449b236b02353b26108a50666be7f2048
Yao Shuai <ys3721@hotmail.com>
2018-12-04 22:33:55 +0800

    【client tools】ppt 课程的 和  飞豆的号

--- FairyGUIBaseViewTool.py
+++ FairyGUIBaseViewTool.py
@@ -1,20 +1,22 @@
 import os
 from xml.dom import minidom
 
-moduleConfig={}
-packagesConfig={}
+moduleConfig = {}
+packagesConfig = {}
+
 
 def createLua(uipropath, luapatch):
     global saveLuaPath
     global uiPath
-    upPath = uipropath
+    uiPath = uipropath
+    saveLuaPath = luapatch;
     resourcePath = saveLuaPath.replace("src/bingogame/view", "resource")
     if os.path.isdir(uipropath):
-        imageTS={}
+        imageTS = {}
         for packageName in os.listdir(uipropath):
             packagePath = os.path.join(uipropath, packageName) + "/package.xml"
-            if packageName[0]!="." and os.path.exists(packagePath):
-                if not moduleConfig.has_key(packageName):
+            if packageName[0] != "." and os.path.exists(packagePath):
+                if packageName not in moduleConfig:
                     moduleConfig[packageName] = {}
                 try:
                     doc = minidom.parse(packagePath)
@@ -22,5 +24,22 @@ def createLua(uipropath, luapatch):
                     pkgid = rootElement.getAttribute("id")
                     packagesConfig[pkgid] = packageName
                     publish = rootElement.getElementsByTagName("publish")[0]
-                    imagelist = rootElement.getElementByTagName("image")
+                    imagelist = rootElement.getElementsByTagName("image")
                     excluded = publish.getAttribute("excluded").split(",")
+                    for image in imagelist:
+                        imageid = image.getAttribute("id")
+                        for exid in excluded:
+                            if exid == imageid:
+                                if "image" == packageName:
+                                    if not os.path.exists(resourcePath + "/image/" + image.getAttribute("path")):
+                                        os.makedirs(resourcePath + "/image/" + image.getAttribute("path"))
+                                    imagePath = packagePath.replace("/package.xml", image.getAttribute("path") + image.getAttribute("name"))
+                                    execCmd()
+                                print("begin to fuck the exclude id of %s, in packageName=%s" %(imageid, packageName))
+                except Exception as e:
+                    print(e)
+
+def execCmd(cmd):
+    return os.popen(cmd).read()
+
+createLua("H:\\h5\\share\\UI\\gameui\\assets", "H:\\h5\\client\\game\\src\\bingogame\\view")
