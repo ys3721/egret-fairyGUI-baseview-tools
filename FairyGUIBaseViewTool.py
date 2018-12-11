636f5c37a1a594bd57c7884f204bb931defa2c93
Yao Shuai <ys3721@hotmail.com>
2018-12-11 14:57:21 +0800

    【egret tools】从assert把image中exclude的image图片复制到了对应的egret workspace的resources目录下。 稍微重构了一些， 把路径提到了全局变量中去。

--- FairyGUIBaseViewTool.py
+++ FairyGUIBaseViewTool.py
@@ -5,15 +5,32 @@ from xml.dom import minidom
 moduleConfig = {}
 packagesConfig = {}
 
+fairy_assets_path = ""
+egret_workspace_root = ""
+egret_workspace_view_path = ""
+egret_workspace_source_path = ""
 
-def generate_base_view(fairyGUI_assets_path, export_workspace_view_path):
-    for package_folder_name in os.listdir(fairyGUI_assets_path):
-        package_file_path = os.path.join(fairyGUI_assets_path, package_folder_name) + "/package.xml"
-        print("Begin read file :" + package_file_path)
-        read_package(package_file_path, export_workspace_view_path, fairyGUI_assets_path, package_folder_name)
+def generate_base_view(fairyGUI_assets_path, export_workspace):
+    global egret_workspace_root
+    egret_workspace_root = export_workspace
+    global fairy_assets_path
+    fairy_assets_path = fairyGUI_assets_path
+    init_paths()
+
+    for package_xml_folder_name in os.listdir(fairy_assets_path):
+        package_xml_file_path = os.path.join(fairyGUI_assets_path, package_xml_folder_name) + "/package.xml"
+        print("Begin read file :" + package_xml_file_path)
+        read_package(package_xml_file_path, package_xml_folder_name)
 
 
-def read_package(package_xml_file_path, workspace_resource_path, pfairyGUI_assets_path, package_folder_name):
+def init_paths():
+    global egret_workspace_view_path
+    egret_workspace_view_path = egret_workspace_root + "/src/bingogame/view"
+    global egret_workspace_source_path
+    egret_workspace_source_path = egret_workspace_root + "/resource"
+
+
+def read_package(package_xml_file_path, package_folder_name):
     doc = minidom.parse(package_xml_file_path)
     root_element = doc.documentElement
     package_id = root_element.getAttribute("id")
@@ -29,17 +46,17 @@ def read_package(package_xml_file_path, workspace_resource_path, pfairyGUI_asset
         for excluded_image_id in excluded_image_ids:
             if excluded_image_id == image_id:
                 if "image" == package_folder_name:
-                    copy_image_to_workspace(workspace_resource_path, pfairyGUI_assets_path, image_element)
+                    copy_image_to_workspace(image_element, package_folder_name)
 
 
-def copy_image_to_workspace(workspace_resource_path, package_xml_path, image_element):
-    target_path = workspace_resource_path + "/image/" + image_element.getAttribute("path")
+def copy_image_to_workspace(image_element, package_folder_name):
+    target_path = egret_workspace_source_path + "/image/" + image_element.getAttribute("path")
     if not os.path.exists(target_path):
         os.makedirs(target_path)
 
-    image_path = package_xml_path+ "/image/" + image_element.getAttribute("path") + image_element.getAttribute("name")
+    image_path = fairy_assets_path + "/" + package_folder_name + image_element.getAttribute("path") + image_element.getAttribute("name")
     copy_image_command = "%s %s%s" % (image_path, target_path, image_element.getAttribute("name"))
-    print("Will copy %s" % (copy_image_command))
+    print("Will copy %s" % copy_image_command)
     shutil.copy(copy_image_command.split(" ")[0], copy_image_command.split(" ")[1])
 
-generate_base_view("H:\\h5\\share\\UI\\gameui\\assets", "H:\\h5\\client_tools\\game\\src\\bingogame\\view")
+generate_base_view("H:\\h5\\share\\UI\\gameui\\assets", "H:\\h5\\client_tools\\game\\")
